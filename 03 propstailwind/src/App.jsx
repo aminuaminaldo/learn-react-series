e5d8b9123d7e1c3eb319b9914f779b47336ac18f
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2024-09-21 19:20:09 +0300

    passing context to the component

--- 03 propstailwind/src/App.jsx
+++ 03 propstailwind/src/App.jsx
@@ -2,13 +2,19 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-
+  let myArr = 
+  [
+    {name: 'John', age: 25, occupation: 'Developer'},
+    {name: 'Jane', age: 30, occupation: 'Designer'},
+  ]
   return (
     <>
       <h1 className="text-3xl bg-green-400 p-3 rounded-md">
         Hello world!
       </h1>
-      <Card />
+      <Card name='Aminaldo' />
+      <Card name='sandro'/>
+      <Card post='pilot' myArr={myArr}/>
     </>
   )
 }
--- 03 propstailwind/src/components/Card.jsx
+++ 03 propstailwind/src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Card() {
+function Card({name = 'Unknown', post='not yet'}) {
   return (
     <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width="100" height="100" />
+        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/73910/mars-mars-rover-space-travel-robot-73910.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" width="100" height="100" />
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
             <blockquote>
             <p className="text-lg font-medium">
@@ -12,10 +12,10 @@ function Card() {
             </blockquote>
             <figcaption className="font-medium">
             <div className="text-sky-500 dark:text-sky-400">
-                Sarah Dayan
+                {name}
             </div>
             <div className="text-slate-700 dark:text-slate-500">
-                Staff Engineer, Algolia
+                {post}
             </div>
             </figcaption>
         </div>
