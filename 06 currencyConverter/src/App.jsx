1f5b944895d2870770d7c64ea8a34a40352413b9
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2024-09-27 20:37:21 +0300

    currency converter
    created a new project this currency converter

---
+++ 06 currencyConverter/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage:
+          "url('https://images.pexels.com/photos/5716052/pexels-photo-5716052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
+      }}
+    >
+      <h1 className="bg-red-200">Hello there</h1>
+    </div>
+  );
+}
+
+export default App;
