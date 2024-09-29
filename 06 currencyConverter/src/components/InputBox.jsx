fc35e3796aafc3936fda363a0565d5df293933f4
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2024-09-29 20:52:02 +0300

    currency converter final edit

--- 06 currencyConverter/src/components/InputBox.jsx
+++ 06 currencyConverter/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useId } from "react";
 
 function InputBox({
   label,
@@ -11,10 +11,13 @@ function InputBox({
   currencyDisabled = false,
   className = "",
 }) {
+  const id = useId();
   return (
     <div className={`bg-white p-3 rounded-lg tex-sm flex${className}`}>
       <div className="w-1-2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label htmlFor={id} className="text-black/40 mb-2 inline-block">
+          {label}
+        </label>
         <input
           type="number"
           className="outline-none w-full bg-transparent py-1.5"
@@ -25,8 +28,24 @@ function InputBox({
             onAmountChange && onAmountChange(Number(e.target.value))
           }
           name=""
-          id=""
+          id={id}
         />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectedCurrency}
+          disabled={currencyDisabled}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          id=""
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
---
+++ 06 currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-09-28/v1/currencies/${currency}.json`)
+        .then((resp) => resp.json())
+        .then((resp) => setData(resp[currency]))
+    },[currency])
+    console.log(data)
+  return data
+}
+
+export default useCurrencyInfo
