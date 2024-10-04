3d6a4ec6b03b60a719b7ee02ee429747efde553c
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2024-10-04 19:26:51 +0300

    add few components to show the react-router-dom

---
+++ 07 reactRounter/src/components/User/User.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function User() {
+  const { userid } = useParams();
+  return (
+    <div className="bg-orange-500 text-black text-3xl text-center py-5">
+      <h1 className="text-5xl text-center">User ID: {userid}</h1>
+    </div>
+  );
+}
+
+export default User;
--- 07 reactRounter/src/main.jsx
+++ 07 reactRounter/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import User from "./components/User/User.jsx";
+import Github from "./components/Github/Github.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="user/" element={<User />}>
+        <Route path=":userid" element={<User />} />
+      </Route>
+      <Route path="contact" element={<Contact />} />
+      <Route path="github" element={<Github />} />
+      <Route path="*" element={<div>Not Found!</div>} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
