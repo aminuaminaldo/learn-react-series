4413d351d8d259850a8dc1cdcf238be017ea1956
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2024-10-04 21:13:14 +0300

    used with useLoaderData hook and loader properties
    in the react-router-dom

--- 07 reactRounter/src/components/Github/Github.jsx
+++ 07 reactRounter/src/components/Github/Github.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  return <div>Github</div>;
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/aminuaminaldo")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setData(data);
+  //         console.log(data);
+  //       });
+  //   }, []);
+
+  const data = useLoaderData();
+  return (
+    <div className="bg-gray-600 text-center text-white m-4 p-4 text-3xl">
+      Github Pulic Repos:{data.public_repos}
+      <img className="rounded-full" src={data.avatar_url} alt="" width={300} />
+    </div>
+  );
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/aminuaminaldo");
+  const data = response.json();
+  return data;
+};
--- 07 reactRounter/src/components/User/User.jsx
+++ 07 reactRounter/src/components/User/User.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 function User() {
   const { userid } = useParams();
   return (
-    <div className="bg-orange-500 text-black text-3xl text-center py-5">
+    <div className="bg-orange-500 text-black text-3xl text-center py-5 m-4">
       <h1 className="text-5xl text-center">User ID: {userid}</h1>
     </div>
   );
