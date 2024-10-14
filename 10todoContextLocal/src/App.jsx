10be66435c7880682a75048402e14dd295a9b3fa
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2024-10-14 19:44:37 +0300

    define the functionalities for todos

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { TodoProvider } from "./contexts";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => {
+      [{ id: Date.now(), ...todo }, ...prev];
+    });
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => {
+      return prev.map((t) => (t.id === id ? { ...t, ...todo } : t));
+    });
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => {
+      return prev.filter((t) => t.id !== id);
+    });
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => {
+      return prev.map((t) =>
+        t.id === id ? { ...t, completed: !t.completed } : t
+      );
+    });
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    </TodoProvider>
   );
 }
 
