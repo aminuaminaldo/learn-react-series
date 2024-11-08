5aa5cbf0d1e0e266f4d0b83f2d3ff5cb558f346c
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2024-11-08 19:58:43 +0300

    completed the integrations redux toolkit and react router dom

--- 11blogApp/src/components/AuthLayout.jsx
+++ 11blogApp/src/components/AuthLayout.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Protected({ children, authentication = true }) {
-  return (
-    <div>
-      <h1>Protected</h1>
-      {children}
-    </div>
-  );
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authentication, authStatus, navigate]);
+
+  return <>{loader ? <div>Loading...</div> : children}</>; // return the children if loader is false
 }
 
 export default Protected;
--- 11blogApp/src/main.jsx
+++ 11blogApp/src/main.jsx
@@ -1,10 +1,85 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import AllPosts from "./pages/AllPosts.jsx";
+import AddPost from "./pages/AddPost.jsx";
+import Post from "./pages/Post.jsx";
+import EditPost from "./pages/EditPost.jsx";
+import Protected from "./components/AuthLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <Protected authentication={false}>
+            <Login />
+          </Protected>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <Protected authentication={false}>
+            <Signup />
+          </Protected>
+        ),
+      },
+      {
+        path: "/posts",
+        element: (
+          <Protected authentication={true}>
+            <AllPosts />
+          </Protected>
+        ),
+      },
+      {
+        path: "/posts/add",
+        element: (
+          <Protected authentication={true}>
+            <AddPost />
+          </Protected>
+        ),
+      },
+      {
+        path: "/posts/:slug",
+        element: (
+          <Protected authentication={true}>
+            <Post />
+          </Protected>
+        ),
+      },
+      {
+        path: "/posts/:slug/edit",
+        element: (
+          <Protected authentication={true}>
+            <EditPost />
+          </Protected>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
